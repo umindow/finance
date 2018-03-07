924afc848e4b3d1166e21dd63effb6c5ed9ec957
levou <251969162@qq.com>
2018-03-07 20:58:32 +0800

    add

--- src/main/java/com/supervise/core/data/in/BankCreditDataImport.java
+++ src/main/java/com/supervise/core/data/in/BankCreditDataImport.java
@@ -16,6 +16,7 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
